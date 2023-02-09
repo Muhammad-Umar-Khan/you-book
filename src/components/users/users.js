81f87940e4b2820e6ceb8bfbc564125f82f91276
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-02-09 12:58:15 +0500

    rendred users listing

--- src/components/users/users.js
+++ src/components/users/users.js
@@ -16,7 +16,41 @@ const Users = () => {
       console.log(error.message);
     }
   }, []);
-  return <Fragment>{users.map((user) => console.log(user))}</Fragment>;
+  return (
+    <Fragment>
+        <form>
+
+      <input type="text" />
+      <button type="submit" className="btn btn-success btn-sm">Search</button>
+        </form>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Update</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                <button className="btn btn-secondary">Update</button>
+              </td>
+              <td>
+                <button className="btn btn-danger">Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
 };
 
 export default Users;
